614fd1170002fde2b214c07e3d5a8a4b7f97b92b
kanaboko <alexandre.biude@hotmail.com>
2017-02-13 17:44:56 -0200

    Classe Produtos

---
+++ Biude.LojaVirtual.Dominio/Entidades/Produtos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biude.LojaVirtual.Dominio.Entidades
+{
+    public class Produtos
+    {
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
