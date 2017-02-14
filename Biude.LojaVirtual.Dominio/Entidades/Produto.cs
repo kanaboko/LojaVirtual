7822516dd4d22e7c98da100211b4c91d1ec6dfbc
kanaboko <alexandre.biude@hotmail.com>
2017-02-14 17:55:39 -0200

    Context

--- Biude.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Biude.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Biude.LojaVirtual.Dominio.Entidades
 {
-    public class Produtos
+    public class Produto
     {
         public int ProdutoID { get; set; }
         public string Nome { get; set; }
---
+++ Biude.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Biude.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biude.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EFDBContext _context = new EFDBContext();
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
